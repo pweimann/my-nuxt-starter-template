2ab25d33bd4f610c663c92f838518f3713335269
paul.weimann <paul.weimann@ista-express.de>
2022-11-22 08:41:37 +0100

    Configure the google font provider

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      Roboto: true,
+      Geramond: true
+    }
+  }
 })
